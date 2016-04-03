c7519be8a83e2fa1c435c7c5bbb306acebb7767f
Raseel Bhagat <raseel.bhagat@zymr.com>
2016-04-03 21:40:27 +0530

    Updated the Help/usage message

--- basher.sh
+++ basher.sh
@@ -8,8 +8,10 @@
 MIN_ARGS=1
 APP=`basename $0`
 
+# Function to get the version of the script
+# Make sure there is a file called VERSION with the 
+# version information about the script
 get_version () {
-    #Make sure there is a file called VERSION with the version information about the script
     FILE=VERSION
     k=1
     echo
@@ -22,13 +24,20 @@ get_version () {
 
 # Put your command-line help here
 usage () {
-    echo -e "\nusage : $APP [-h|--help] [-v|--version] [-r|--required 123] [-o|--optional [124]]\n"
+    echo -e "\n$APP : Boilerplate code to start writing bash scripts similar to most Linux command-line utilities"
+    echo -e "Usage :"
+    echo -e "\t$APP [-h|--help] [-v|--version] [-r|--required 123] [-o|--optional [124]]"
+    echo -e "where,"
+    echo -e "\t-r, --required <arg> : If this flag is set, an argument is mandatory"
+    echo -e "\t-o, --option [arg] : If this flag is set, an argument is optional"
+    echo -e "\t-v, --version : Show script version info"
+    echo -e "\t-h, --help : Show this Help message"
 }
 
 # If the minimum specified options are not provided, we walk
 if [[ $# < $MIN_ARGS ]];
 then
-    echo "Too few arguments"
+    echo "Error : Too few arguments"
     usage
     exit 1
 fi
