54eac59db4985634a5a97e28271573a0493d8549
screwgoth <raseelbhagat@gmail.com>
2017-02-14 12:06:14 +0530

    Added some color to the script

--- basher.sh
+++ basher.sh
@@ -18,22 +18,22 @@ get_version () {
 # Put your command-line help here
 usage () {
     get_version
-    echo -e "\n$APP : Boilerplate code to start writing bash scripts similar to most Linux command-line utilities"
-    echo -e "Usage :"
-    echo -e "\t$APP [-h|--help] [-v|--version] [-r|--required 123] [-o|--optional [124]]"
-    echo -e "\t$APP -r|--required <argument>"
-    echo -e "\t$APP -o|--optional [optional_argument]"
-    echo -e "where,"
-    echo -e "\t-r, --required <arg> : If this flag is set, an argument is mandatory"
-    echo -e "\t-o, --option [arg] : If this flag is set, an argument is optional"
-    echo -e "\t-v, --version : Show script version info"
-    echo -e "\t-h, --help : Show this Help message"
+    echo -e "\n\033[34m$APP : Boilerplate code to start writing bash scripts similar to most Linux command-line utilities"
+    echo -e "\033[34mUsage :"
+    echo -e "\t\033[34m$APP [-h|--help] [-v|--version] [-r|--required 123] [-o|--optional [124]]\033[0m"
+    echo -e "\t\033[34m$APP -r|--required <argument>"
+    echo -e "\t\033[34m$APP -o|--optional [optional_argument]"
+    echo -e "\033[34mwhere,"
+    echo -e "\t\033[34m-r, --required <arg> : If this flag is set, an argument is mandatory"
+    echo -e "\t\033[34m-o, --option [arg] : If this flag is set, an argument is optional"
+    echo -e "\t\033[34m-v, --version : Show script version info"
+    echo -e "\t\033[34m-h, --help : Show this Help message\033[0m"
 }
 
 # If the minimum specified options are not provided, we walk
 if [[ $# < $MIN_ARGS ]];
 then
-    echo "Error : Too few arguments"
+    echo -e "\033[31mError : Too few arguments\033[0m"
     usage
     exit 1
 fi
@@ -41,7 +41,7 @@ fi
 OPTIONS=`getopt -o vho::r: --long version,help,optional::,required: -n '$APP' -- "$@"`
 if [ $? != 0 ];
 then
-    echo "Error Parsing arguments"
+    echo -e "\033[31mError Parsing arguments\033[0m"
     usage
     exit 1
 fi
@@ -77,7 +77,7 @@ while true; do
         -r | --required)
             case "$2" in
                 "")
-                    echo "Argument Missing"
+                    echo -e "\033[31mArgument Missing. It is mandatory to provide an argument for this flag\033[0m"
                     exit 1
                     break;;
                 *)
@@ -108,14 +108,14 @@ if [ -z $ARG_OPTIONAL ];
 then
     echo "Optional Argument not set" > /dev/null
 else
-    echo -e "Optional Argument is set as $ARG_OPTIONAL"
+    echo -e "\033[32mOptional Argument is set as $ARG_OPTIONAL\033[0m"
 fi
 
 if [ -z $ARG_REQUIRED ];
 then
     echo "Required Argument not set" > /dev/null
 else
-    echo "Required Argument is set as $ARG_REQUIRED"
+    echo -e "\033[32mRequired Argument is set as $ARG_REQUIRED\033[0m"
 fi
 
 exit 0
